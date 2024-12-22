9e87b582b18af5304a7984b41755f11047f15eb7
feffy380 <114889020+feffy380@users.noreply.github.com>
2024-12-22 20:59:25 +0100

    fix: zero terminal snr rescale betas before caching snr

--- train_network.py
+++ train_network.py
@@ -242,11 +242,9 @@ class NetworkTrainer:
 
     def get_noise_scheduler(self, args: argparse.Namespace, device: torch.device) -> Any:
         noise_scheduler = DDPMScheduler(
-            beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000, clip_sample=False
+            beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000, clip_sample=False, rescale_betas_zero_snr=args.zero_terminal_snr
         )
         prepare_scheduler_for_custom_training(noise_scheduler, device)
-        if args.zero_terminal_snr:
-            custom_train_functions.fix_noise_scheduler_betas_for_zero_terminal_snr(noise_scheduler)
         return noise_scheduler
 
     def encode_images_to_latents(self, args, vae: AutoencoderKL, images: torch.FloatTensor) -> torch.FloatTensor:
